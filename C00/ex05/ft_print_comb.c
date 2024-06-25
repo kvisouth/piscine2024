8d78dddee026f538d8d3dfdbdf16f91093ad9f38
Kevin Visouthivong <kevisout@paul-f4Ar1s1.clusters.42paris.fr>
2024-06-25 17:20:34 +0200

    trimmed fct

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -18,22 +18,6 @@ void	ft_putchar(char c)
 }
 
 /*
-Le '+48' est une conversion de la POSITION sur le tableau ASCII vers
-la valeur du chiffre en question sur le tableau ASCII.
-
-La condition fait en sorte d'afficher un espacement ', ' tant que
-notre nombre n'EST PAS '789'
-*/
-void	ft_putchar_multiple(char x, char y, char z)
-{
-	ft_putchar(x + 48);
-	ft_putchar(y + 48);
-	ft_putchar(z + 48);
-	if (x != 7 || y != 8 || z != 9)
-		write(1, ", ", 2);
-}
-
-/*
 
 */
 void	ft_print_comb(void)
@@ -51,7 +35,11 @@ void	ft_print_comb(void)
 			z = y + 1;
 			while (z <= 9)
 			{
-				ft_putchar_multiple(x, y, z);
+				ft_putchar(x + 48);
+				ft_putchar(y + 48);
+				ft_putchar(z + 48);
+				if (x != 7 || y != 8 || z != 9)
+					write(1, ", ", 2);
 				z++;
 			}
 			y++;
@@ -60,7 +48,7 @@ void	ft_print_comb(void)
 	}
 }
 
-int	main(void)
-{
-	ft_print_comb();
-}
+// int	main(void)
+// {
+// 	ft_print_comb();
+// }
