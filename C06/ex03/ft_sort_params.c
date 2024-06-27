30f1ff07bcc798200f9098d818b97a088373ee7c
Kevin Visouthivong <kevisout@paul-f3Ar7s4.clusters.42paris.fr>
2024-06-27 15:25:22 +0200

    working

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-06-27 12:18:25 by kevisout          #+#    #+#             */
+/*   Updated: 2024/06/27 14:45:23 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	if (argc < 3)
+		return (0);
+	while (i < argc)
+	{
+		if (argv[i + 1] == NULL)
+			break ;
+		if (argv[i][0] > argv[i + 1][0])
+		{
+			tmp = argv[i];
+			argv[i] = argv [i + 1];
+			argv[i + 1] = tmp;
+			i = 0;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i++]);
+		ft_putchar('\n');
+	}
+}
