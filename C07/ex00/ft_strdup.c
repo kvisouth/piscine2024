9c699ba05b27b97c5c212ab9877867e2834e108b
Kevin Visouthivong <kevisout@paul-f4Ar7s2.clusters.42paris.fr>
2024-06-28 11:29:54 +0200

    strdup

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-06-28 09:14:46 by kevisout          #+#    #+#             */
+/*   Updated: 2024-06-28 09:14:46 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*str;
+
+	len = ft_strlen(src);
+	str = malloc(len + 1);
+	str = ft_strcpy(str, src);
+	return (str);
+}
