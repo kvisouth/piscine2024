222934357967559b8e87f55db194cbf6f246dcb4
Kevin Visouthivong <kevisout@paul-f4Ar8s1.clusters.42paris.fr>
2024-07-03 14:57:50 +0200

    factorial

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -1,48 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kevisout <kevisout@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-07-03 10:45:06 by kevisout          #+#    #+#             */
-/*   Updated: 2024-07-03 10:45:06 by kevisout         ###   ########.fr       */
+/*   Created: 2024-07-03 12:39:18 by kevisout          #+#    #+#             */
+/*   Updated: 2024-07-03 12:39:18 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/*
-Factorielle exemples :
-nb = 2!
-1x2
-
-nb = 5!
-1x2x3x4x5 = 120
-
-nb = 6!
-1x2x3x4x5x6 = 720 (c'est 120x6)
-*/
-int	ft_iterative_factorial(int nb)
+int	ft_iterative_power(int nb, int power)
 {
 	int	i;
-	int	x;
+	int	n;
 
 	i = 1;
-	x = 1;
-	if (nb < 0)
+	n = nb;
+	if (power < 0)
 		return (0);
-	if (nb == 1 || nb == 0)
+	if (power == 0)
 		return (1);
-	while (i++ < nb)
-		x *= i + 1;
-		i++;
-	return (x);
+	while (i++ < power)
+		nb = nb * n;
+	return (nb);
 }
 
 // int main (int ac, char **av)
 // {
 // 	if (ac > 1)
-// 		printf("%d\n", ft_iterative_factorial(atoi(av[1])));
+// 		printf("resultat :%d\n", ft_iterative_power(atoi(av[1]), atoi(av[2])));
 // }
