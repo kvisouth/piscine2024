d0dbc96e93d852e69ef24748271e90d5d2ecf4b7
Kevin Visouthivong <kevisout@paul-f4Ar8s1.clusters.42paris.fr>
2024-07-04 13:44:18 +0200

    explications

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -15,28 +15,10 @@
 
 #include <unistd.h>
 
-/*
-Wow, what's 'enum' ?
-We use enums to avoid using magic numbers and macros.
-It's a way to give a name to a set of values.
-It just makes things easier to read and understand.
-In this case for example, in t_lex, word = "ls" and token = WORD.
-Instead of doing something like : word = "ls" and token = 1
-I could write in comment 1 = WORD but it would be less understandable
-in the rest of the code.
-In the rest of the code we will be able to use this like :
-if (t_lex->token == WORD)
-	printf("It's a word !\n");
-*/
-typedef enum s_bool
+typedef s_bool
 {
-	TRUE,
-	FALSE,
-	EVEN,
-	EVEN_MSG,
-	ODD_MSG,
-	SUCCESS,
-}	t_bool;
+	
+}	t_bool
 
 void	ft_putstr(char *str);
 t_bool	ft_is_even(int nbr);
