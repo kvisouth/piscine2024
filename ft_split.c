171ddfe1267be2fa3f924157090d5e171f4d037a
kvisouth <visouthivong.kevin@gmail.com>
2024-07-08 17:54:47 +0000

    d

---
+++ ft_split.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 17:41:47 by ubuntu            #+#    #+#             */
+/*   Updated: 2024/07/08 17:50:35 by ubuntu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int count_words(char *str, char *sep)
+{
+    int i;
+    int j;
+    int cpt;
+
+    i = 0;
+    cpt = 0;
+    while (str[i])
+    {
+        while
+    }
+    
+}
+
+
+char    **ft_split(char *str, char *charset)
+{
+
+}
