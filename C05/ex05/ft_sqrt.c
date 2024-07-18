c53caf8f5fe06189f471a90e984a0cb339b90636
Ubuntu <ubuntu@vps-866f53e7.vps.ovh.net>
2024-07-18 21:50:56 +0000

    normed

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -31,13 +31,3 @@ int	ft_sqrt(int nb)
 	}
 	return (0);
 }
-
- #include <stdio.h>
- #include <stdlib.h>
- int main(int ac, char **av)
- {
- 	if (ac == 2)
- 	{
- 		printf("%d\n", ft_sqrt(atoi(av[1])));
- 	}
- }
